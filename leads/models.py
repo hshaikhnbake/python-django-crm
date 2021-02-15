238a02d10bd9ac226ba984a99f4cdec623aa18e5
Haris Shaikh <shaikh.m.haris@gmail.com>
2021-02-15 11:04:22 -0500

    set custom user models

---
+++ leads/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
+
+
+class Lead(models.Model):
+
+    # SOURCE_CHOICES = (
+    #     ('Facebook', 'Facebook'),
+    #     ('Instagram', 'Instagram'),
+    #     ('Open House', 'Open House'),
+    #     ('Circle of Influence', 'Circle of Influence'),
+    # )
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
+    # phoned = models.BooleanField(default=False)
+    # source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+
+    # profile_picture = models.ImageField(blank=True, null=True)
+    # special_filies = models.FileField(blank=True, null=True)
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
