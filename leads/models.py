363faaccfb60e60d35b184341968e143c184412f
Haris Shaikh <shaikh.m.haris@gmail.com>
2021-02-16 17:16:59 -0500

    created form field for leads

--- leads/models.py
+++ leads/models.py
@@ -26,8 +26,12 @@ class Lead(models.Model):
     # profile_picture = models.ImageField(blank=True, null=True)
     # special_filies = models.FileField(blank=True, null=True)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.user.email
